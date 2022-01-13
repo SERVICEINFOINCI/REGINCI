012de85f05f1d8832ceee6bac2bdc6bbc5f22e40
reginci <projet.inci@gmail.com>
2022-01-13 15:13:35 +0000

    connecte

--- REGINCI/insert_client_new.php
+++ REGINCI/insert_client_new.php
@@ -5,7 +5,7 @@ require('connexion_reginci.php');
 //recupérer les information du formulaire
 
 
-$Matricule= $_POST['Mat_Client'];
+$Matricule= $_POST['Matricule_Client'];
 $Prenom = $_POST['Prenom_Client'];
 $Nom = $_POST['Nom_Client'];
 $Contact =$_POST['Contact_Client'];
@@ -13,7 +13,7 @@ $Contact =$_POST['Contact_Client'];
 echo $Matricule.' '.$Prenom.$Nom.$Contact;
 
 //On insère les données reçues dans la table
-$client = $dbco->prepare ("INSERT INTO client (Mat_Client,Prenom_Client, Nom_Client, Contact_Client)
+$client = $dbco->prepare ("INSERT INTO client (Matricule_Client,Prenom_Client, Nom_Client, Contact_Client)
 VALUES(?, ?, ?,?)");
 $client->execute (array ($Matricule,$Prenom,$Nom,$Contact));
 header("Location:merci.html");
