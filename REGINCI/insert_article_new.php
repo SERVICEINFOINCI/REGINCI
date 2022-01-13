2e54bd303dc612ba59042a4dc1fc4abc83bd2428
reginci <projet.inci@gmail.com>
2022-01-13 12:37:06 +0000

    ok

--- REGINCI/insert_article_new.php
+++ REGINCI/insert_article_new.php
@@ -9,11 +9,12 @@ $Reference_article = $_POST['Reference_article'];
 $Designation_article = $_POST['Designation_article'];
 $Prix_unitaire = $_POST['Prix_unitaire'];
 $Id_Stock =$_POST['Id_Stock'];
+$Id_Client=$_POST['Id_Client'];
 
 //On insère les données reçues dans la table
-$article = $dbco->prepare("INSERT INTO article (Reference_Article, Designation_Article, Prix_Unitaire, Id_Stock)
-VALUES(?, ?, ?,?)");
-$article->execute(array($Reference_article, $Designation_article, $Prix_unitaire, $Id_Stock));
+$article = $dbco->prepare("INSERT INTO article (Reference_Article, Designation_Article, Prix_Unitaire, Id_Stock,Id_Client)
+VALUES(?, ?, ?,?,?)");
+$article->execute(array($Reference_article, $Designation_article, $Prix_unitaire, $Id_Stock,$Id_Client));
 header("Location:merci.html");
 
 
