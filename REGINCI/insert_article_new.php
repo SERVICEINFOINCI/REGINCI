c83f36f382352c2a1f74f3d3f2936ee689d1a81b
reginci <projet.inci@gmail.com>
2022-01-19 10:48:22 +0000

    OK

--- REGINCI/insert_article_new.php
+++ REGINCI/insert_article_new.php
@@ -5,16 +5,16 @@ $user = "root";
 $pass = "";
 //recuperer les information du formulaire
 
-$Reference_article = $_POST['Reference_article'];
+$Reference_article = $_POST['Ref_article'];
 $Designation_article = $_POST['Designation_article'];
 $Prix_unitaire = $_POST['Prix_unitaire'];
 $Id_Stock =$_POST['Id_Stock'];
 $Id_Client=$_POST['Id_Client'];
 
 //On insère les données reçues dans la table
-$article = $dbco->prepare("INSERT INTO article (Reference_Article, Designation_Article, Prix_Unitaire, Id_Stock,Id_Client)
+$article = $dbco->prepare("INSERT INTO article (Ref_Article, Designation_Article, Prix_Unitaire, Id_Stock,Id_Client)
 VALUES(?, ?, ?,?,?)");
-$article->execute(array($Reference_article, $Designation_article, $Prix_unitaire, $Id_Stock,$Id_Client));
+$article->execute(array($Ref_article, $Designation_article, $Prix_unitaire, $Id_Stock,$Id_Client));
 header("Location:merci.html");
 
 
